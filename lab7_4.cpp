a793419ecf8d303d1c26ff00e1b62a38e1f7848b
suwichada <jippee.1211@gmail.com>
2021-11-30 17:37:12 +0700

    เสร็จ

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,17 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    double a;
+	int i = 0;
+    cout << "Please input GPA: ";
+	cin >> a;
+
+	while(i < 10){
+		if(gpa[i] >= a){
+			cout << id[i] << " " << name[i] << " " << gpa[i] << "\n";
+		}
+		i++;
+	}
 		
 	return 0;
 }
